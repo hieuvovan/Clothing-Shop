e104f94bce16c7f56920ddcf747283a7d66570d8
Hieuvovan <hieuvo0904@gmail.com>
2021-03-05 18:34:52 +0700

    update shop page, collection preview component, add collection item

--- src/components/collection-preview/collection-preview.component.jsx
+++ src/components/collection-preview/collection-preview.component.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import "./collection-component.style.scss";
+import CollectionItem from '../collection-item/collection-item.component'
 
 const CollectionPreview = ({ title, items }) => {
   return (
@@ -8,8 +9,8 @@ const CollectionPreview = ({ title, items }) => {
       <div className="preview">
         {items
           .filter((item, idx) => idx < 4)
-          .map((item) => {
-            return <div key={item.id}>{item.name}</div>;
+          .map(({id,...otheritem}) => {
+            return <CollectionItem key={id} {...otheritem} />;
           })}
       </div>
     </div>
