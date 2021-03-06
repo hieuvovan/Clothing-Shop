bbf4585ebc6f2410dbc49c292ffbea546b3db9bd
Hieuvovan <hieuvo0904@gmail.com>
2021-03-06 18:34:59 +0700

    update sign in page with input form

--- src/App.js
+++ src/App.js
@@ -2,16 +2,18 @@ import "./App.css";
 import HomePage from "./pages/homepage/homepage.component";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
 import ShopPage from "./pages/shop/shop.component.jsx";
-import Header from './/components/header/header.component'
+import Header from ".//components/header/header.component";
+import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
-        <Header/>
+        <Header />
         <Switch>
           <Route exact path="/" component={HomePage} />
           <Route exact path="/shop" component={ShopPage} />
+          <Route exact path="/signin" component={SignInAndSignUpPage} />
         </Switch>
       </BrowserRouter>
     </div>
